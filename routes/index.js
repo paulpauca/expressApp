db5d03d0ebfbbdae1d8663a3c13a8697d75fd9bb
Paul Pauca <paucavp@wfu.edu>
2018-03-25 23:16:36 -0400

    forcing deployment to Heroku

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', sam);
+
+function sam(req, res, next) {
 	res.render('index', 
 		{title: "Best Movie Characters of all Time"});
-});
+}
 
 module.exports = router;
